6124286fdaaf49477303ceceff4f28cfd2ef0009
germanmauro <mygsystems@gmail.com>
2021-02-07 13:53:10 -0300

    cambios

--- resources/views/cursos/listado.blade.php
+++ resources/views/cursos/listado.blade.php
@@ -71,7 +71,7 @@
                         @if (session()->has("Perfil") && session("Perfil")=="alumno"){{-- Solo si es alumno --}}
                             <a class="curso-favorite" title='Agregar a favoritos'><i class='fa fa-heart'></i></a>
                         @endif
-                        <div style="float: right">
+                        <div>
                           <a class="curso-compartir" title="Compartir por Whatsapp" href="whatsapp://send?text=http://capacitacionee.com/Cursos/{{$item->id}}/{{$item->slug()}}" data-text="{{$item->titulo}}" data-action="share/whatsapp/share"><i class='fab fa-whatsapp-square'></i></a>
                           <a class="curso-compartir" title="Compartir por email" href="mailto:?subject=Quiero compartirte este curso&amp;body=Mirá este curso {{$item->titulo}} http://capacitacionee.com/Cursos/{{$item->id}}/{{$item->slug()}}"><i class='fas fa-envelope'></i></a>
                         </div>
