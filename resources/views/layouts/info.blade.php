6ffd5cbab15d2229580259a25cb69d6fa5145bb1
germanmauro <mygsystems@gmail.com>
2021-02-13 01:59:52 -0300

    diseño

--- resources/views/layouts/info.blade.php
+++ resources/views/layouts/info.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('header')
     <div id="banner-buscador">
-      <h2 class="titulo">Cursos personales online y en español</h2>
+      <h2>Cursos personales online y en español</h2>
       <form class="buscador" action="/Cursos/Filter" method="GET">
         <input type="text" autocomplete="off" placeholder="ENCUENTRA CURSOS Y PROFESORES" name="filter">
         <button type="submit"><i class="fa fa-search"></i></button>
