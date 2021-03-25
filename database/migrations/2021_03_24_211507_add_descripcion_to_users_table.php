6e5d701a158e5f157cba38ca0b71a7df4c785fd7
germanmauro <mygsystems@gmail.com>
2021-03-25 01:06:32 -0300

    cambio campos mysql

--- database/migrations/2021_03_24_211507_add_descripcion_to_users_table.php
+++ database/migrations/2021_03_24_211507_add_descripcion_to_users_table.php
@@ -14,7 +14,7 @@ class AddDescripcionToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text("descripcion")->default("");
+            $table->text("descripcion");
         });
     }
 
