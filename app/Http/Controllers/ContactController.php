ee984c253d09f5c351138a118b470124cc6f7abb
germanmauro <mygsystems@gmail.com>
2021-03-31 23:56:07 -0300

    contacto

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\mailContact;
+use App\Mail\notificationMessage;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -17,7 +18,7 @@ class ContactController extends Controller
         $contacto->email = $request->email;
         $contacto->mensaje = $request->mensaje;
         $contacto->telefono = $request->telefono;
-        Mail::to($contacto->email)->send(new mailContact($contacto));
+        Mail::to("info@capacitacionee.com")->send(new notificationMessage("hola","como anda","mensaje"));
         return redirect('')->with('success','Mensaje enviado con éxito, en breve le responderemos');
     }
 }
