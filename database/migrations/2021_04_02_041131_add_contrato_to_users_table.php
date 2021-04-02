3ef4d0bcf92997e349c0bba33218f68cd27a02c1
germanmauro <mygsystems@gmail.com>
2021-04-02 12:54:12 -0300

    carga de datos

--- database/migrations/2021_04_02_041131_add_contrato_to_users_table.php
+++ database/migrations/2021_04_02_041131_add_contrato_to_users_table.php
@@ -14,7 +14,7 @@ class AddContratoToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text("contrato")->default("");
+            $table->text("contrato")->nullable();
         });
     }
 
